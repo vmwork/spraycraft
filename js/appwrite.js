67494adf3502b5a6b15317070bb4f5fe0baa85be
vmwork <v.marwork@gmail.com>
2025-04-30 11:09:27 +0300

    fix

--- js/appwrite.js
+++ js/appwrite.js
@@ -11,52 +11,74 @@ const result = await databases.listDocuments(
   "680e235f00009982dde6", // databaseId
   "680e26a100097b97eebd" // collectionId
 );
-console.log(result);
+let products = result?.documents;
 let element = document.getElementById("cards");
-for (let i = 0; i < result.documents.length; i++) {
-  let div = document.createElement("div");
-  div.className = "product-card";
-  div.innerHTML = `<img src="${result.documents[i].url}" alt="copter" width="200" />
-          <p>${result.documents[i].product_name}</p> 
-          <div class="product-card-price">
-            <p>${result.documents[i].price} грн</p>
 
-          <button  class="product-card-buttons">   <img name="${result.documents[i].$id}" src="/img/back.png" alt="" width="25" /></button>   
-          </div>`;
+const reactiveUpdateFilter = () => {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+  for (let i = 0; i < products.length; i++) {
+    let div = document.createElement("div");
+    div.className = "product-card";
+    div.innerHTML = `<img src="${products[i].url}" alt="copter" width="200" />
+                <p class="product-card-name">${products[i].product_name}</p> 
+                <div class="product-card-artical"><p> Артикул : ${products[i].artical}</p></div>
+                  
+                <div class="product-card-price">
+                  <p>${products[i].price} грн</p>
+      
+                <button  class="product-card-buttons">   <img class="img-button" name="${products[i].$id}" src="/img/add-to-cart.png" alt="" width="25" /></button>   
+                </div>`;
 
-  element.append(div);
+    element.append(div);
 
-  if (i === result.documents.length - 1) {
-    const buttons = document.querySelectorAll(".product-card-buttons");
-    buttons.forEach((button) => {
-      button.addEventListener("click", (e) => {
-        const cart = getCartState();
-        let productToCard = [];
-        const getProduct = () => {
-          result.documents.forEach((product) => {
-            if (product.$id === e.target.name) {
-              if (product.product_count_to_buy === null) {
-                product.product_count_to_buy = 1;
-              } else {
-                product.product_count_to_buy++;
+    if (i === products.length - 1) {
+      const buttons = document.querySelectorAll(".img-button");
+      buttons.forEach((button) => {
+        button.addEventListener("click", (e) => {
+          const cart = getCartState();
+          let productToCard = [];
+          const getProduct = () => {
+            products.forEach((product) => {
+              if (product.$id === e.target.name) {
+                if (product.product_count_to_buy === null) {
+                  product.product_count_to_buy = 1;
+                } else {
+                  product.product_count_to_buy++;
+                }
+                productToCard = product;
               }
-              productToCard = product;
+            });
+            let coincidence = false;
+            cart.forEach((product, i) => {
+              if (product.$id === productToCard.$id) {
+                cart[i] = productToCard;
+                coincidence = true;
+              }
+            });
+            if (cart.length === 0 || !coincidence) {
+              cart.push(productToCard);
             }
-          });
-          let coincidence = false;
-          cart.forEach((product, i) => {
-            if (product.$id === productToCard.$id) {
-              cart[i] = productToCard;
-              coincidence = true;
-            }
-          });
-          if (cart.length === 0 || !coincidence) {
-            cart.push(productToCard);
-          }
-          setCartState(cart);
-        };
-        getProduct();
+            setCartState(cart);
+          };
+          getProduct();
+        });
       });
+    }
+  }
+};
+
+reactiveUpdateFilter();
+let filter = document.getElementById("products-filter");
+filter.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  if (e.target.value === "Усі") {
+    products = result?.documents;
+  } else {
+    products = result?.documents.filter((product) => {
+      return product.product_name === e.target.value;
     });
   }
-}
+  reactiveUpdateFilter();
+});
